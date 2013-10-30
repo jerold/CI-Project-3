cb270d61a673220fdd63f2d0507979a0aa98920f
jerold <jerold@generalui.com>
2013-10-30 16:55:44 -0600

    parser updated for 3 of 4 current datasets

--- patternParser.py
+++ patternParser.py
@@ -6,131 +6,103 @@ import itertools
 import math
 import random
 
-optdigits = {'inFiles':['data/optdigits/optdigits.tra',
-                        'data/optdigits/optdigits.tes'],
-             'outFile':'data/optdigits/optdigits.json',
-             'width':8,
-             'height':8}
-
-optdigitsOrig = {'inFiles':['data/optdigits/optdigits-orig.tra',
-                        'data/optdigits/optdigits-orig.windep',
-                        'data/optdigits/optdigits-orig.wdep'],
-             'outFile':'data/optdigits/optdigits-orig.json',
-             'width':32,
-             'height':32}
-
-letterRecognition = {'inFiles':['data/letter/letter-recognition.data'],
-             'outFile':'data/letter/letter-recognition.json',
-             'width':16,
+pageBlock = {'inFiles':['data/block/page-blocks.data'],
+             'outFile':'data/block/pageblocks.json',
+             'width':10,
              'height':1}
 
-pendigits = {'inFiles':['data/pendigits/pendigits.tra',
-                        'data/pendigits/pendigits.tes'],
-             'outFile':'data/pendigits/pendigits.json',
-             'width':16,
+car = {'inFiles':['data/car/car.data'],
+             'outFile':'data/car/car.json',
+             'width':10,
              'height':1}
 
-semeion = {'inFiles':['data/semeion/semeion.data'],
-             'outFile':'data/semeion/semeionTT.json',
-             'width':16,
-             'height':16}
+flare = {'inFiles':['data/flare/flare.data1',
+                    'data/flare/flare.data2'],
+             'outFile':'data/flare/flare.json',
+             'width':10,
+             'height':1}
 
-def parseOptdigits(lines, w, h):
+flare = {'inFiles':['data/houseing/houseing.data'],
+             'outFile':'data/flare/flare.json',
+             'width':10,
+             'height':1}
+
+
+def parseFlare(lines):
     patSet = []
+    attributes = [[] for _ in range(10)]
+    attributesAlt = [{'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'H':7},
+                     {'X':1, 'R':2, 'S':3, 'A':4, 'H':5, 'K':6},
+                     {'X':1, 'O':2, 'I':3, 'C':4}]
+    targets = [[] for _ in range(3)]
     for line in lines:
         line = line.split('\n')[0]
-        line = line.split(',')
-        pattern = list(mygrouper(w, line[:len(line)]))
-        patternTarget = pattern[h][0]
-        patternTarget = int(patternTarget)
-        pattern = pattern[:h]
-        for i in range(len(pattern)):
-            for j in range(len(pattern[i])):
-                pattern[i][j] = int(pattern[i][j])
+        line = line.split()
+        pattern = line[:-3]
+        for i, elem in enumerate(pattern):
+            if i < 3:
+                pattern[i] = attributesAlt[i][elem]
+            else:
+                pattern[i] = int(elem)
+            attributes[i].append(pattern[i])
+        patternTarget = line[-3:]
         patSet.append({'p':pattern, 't':patternTarget})
+        targets[0].append(patternTarget[0])
+        targets[1].append(patternTarget[1])
+        targets[2].append(patternTarget[2])
+        #print('p:' + str(pattern) + '  t:' + str(patternTarget))
+    print(set(targets[0]))
+    print(set(targets[1]))
+    print(set(targets[2]))
+    print("Attributes")
+    for attribute in attributes:
+        print(set(attribute))
     return patSet
 
-def parseOptdigitsOrig(lines, w, h):
+def parseCar(lines):
     patSet = []
-    pattern = []
-    patternTarget = 0
-    hi = 0
-    for line in lines:
-        line = list(line)
-        # Cuts off newline \n
-        line = line[:len(line)-1]
-        if len(line) == w:
-            # Pattern Line
-            for i in range(len(line)):
-                line[i] = int(line[i])
-            pattern.append(line)
-            hi = hi + 1
-        elif hi == h:
-            # End of Pattern and Target Line
-            patternTarget = int(''.join(line))
-            #patternTarget = ''.join(line)
-            patSet.append({'p':pattern, 't':patternTarget})
-            # print('Target:' + str(''.join(line)))
-            # var = input("Cont?" + str(hi))
-            pattern = []
-            hi = 0
-        else:
-            # Bad line
-            #print('Bad Line: [' + str(''.join(line)) + ']')
-            pattern = []
-            hi = 0
-    return patSet
-
-def parseLetterRecognition(lines):
-    patSet = []
-    for line in lines:
-        line = line.split('\n')[0]
-        line = line.split(',')
-        pattern = line[1:]
-        for i in range(len(pattern)):
-            pattern[i] = int(pattern[i])
-        patternTarget = line[0]
-        patSet.append({'p':pattern, 't':patternTarget})
-    return patSet
-
-def parsePendigits(lines):
-    patSet = []
+    attributes = [[] for _ in range(6)]
+    attributesAlt = [{'low':1, 'vhigh':4, 'med':2, 'high':3},
+                     {'low':1, 'vhigh':4, 'med':2, 'high':3},
+                     {'4':3, '5more':4, '2':1, '3':2},
+                     {'4':2, 'more':3, '2':1},
+                     {'big':3, 'med':2, 'small':1},
+                     {'med':2, 'low':1, 'high':3}]
+    targets = []
+    targetsAlt = {'acc':2, 'good':3, 'unacc':1, 'vgood':4}
     for line in lines:
         line = line.split('\n')[0]
         line = line.split(',')
         pattern = line[:len(line)-1]
-        for i in range(len(pattern)):
-            pattern[i] = int(pattern[i])
-        patternTarget = int(line[len(line)-1])
+        for i, elem in enumerate(pattern):
+            pattern[i] = attributesAlt[i][elem]
+            attributes[i].append(pattern[i])
+        patternTarget = targetsAlt[line[len(line)-1]]
         patSet.append({'p':pattern, 't':patternTarget})
+        targets.append(patternTarget)
+        #print('p:' + str(pattern) + '  t:' + str(patternTarget))
+    print(set(targets))
+    print("Attributes")
+    for attribute in attributes:
+        print(set(attribute))
     return patSet
 
-def parseSemeion(lines, w, h):
+def parseBlock(lines):
     patSet = []
+    targets = []
     for line in lines:
         line = line.split('\n')[0]
-        line = line.split(' ')
-        pattern = list(mygrouper(w, line[:len(line)-1]))
-        patternTarget = pattern[h]
-        pattern = pattern[:h]
-        if len(patternTarget) == 10:
-            for i in range(len(pattern)):
-                for j in range(len(pattern[i])):
-                    pattern[i][j] = int(pattern[i][j].split('.')[0])
-            for i in range(len(patternTarget)):
-                patternTarget[i] = int(patternTarget[i])
-            #print(patternTarget)
-            patternTarget = list(itertools.compress([0,1,2,3,4,5,6,7,8,9], patternTarget))[0]
-            #print(line)
-            #for i in range(len(pattern)):
-            #    print(''.join(str(x) for x in pattern[i]))
-            #print(patternTarget)
-            #var = input("cont")
-            patSet.append({'p':pattern, 't':patternTarget})
-        else:
-            var = input("Bad line [" + line + "]")
-    random.shuffle(patSet)
+        line = line.split()
+        pattern = line[:len(line)-1]
+        for i in range(len(pattern)):
+            pattern[i] = float(pattern[i])
+        patternTarget = int(line[len(line)-1])
+        patSet.append({'p':pattern, 't':patternTarget})
+        targets.append(patternTarget)
+        #print('p:' + str(pattern) + '  t:' + str(patternTarget))
+    print(set(targets))
     return patSet
+
 
 def mygrouper(n, iterable):
     "http://stackoverflow.com/questions/1624883/alternative-way-to-split-a-list-into-groups-of-n"
@@ -309,36 +281,27 @@ def printPattern(pattern):
 
 
 if __name__=="__main__":
-    #parseSet = optdigits
-    #parseSet = optdigitsOrig
-    #parseSet = letterRecognition
-    #parseSet = pendigits
-    parseSet = semeion
+    #parseSet = pageBlock
+    #parseSet = car
+    parseSet = flare
+    
     lines = []
     for fileName in parseSet['inFiles']:
         with open(fileName) as file:
             fileLines = file.readlines()
             for line in fileLines:
                 lines.append(line)
-    #patternSet = parseOptdigits(lines, parseSet['width'], parseSet['height'])
-    #patternSet = parseOptdigitsOrig(lines, parseSet['width'], parseSet['height'])
-    #patternSet = parseLetterRecognition(lines)
-    #patternSet = parsePendigits(lines)
-    patternSet = parseSemeion(lines, parseSet['width'], parseSet['height'])
-    
-    # buildKMeansCenters(patternSet, parseSet['width'], parseSet['height'], 20)
-    centerSigmas = buildCentersAndSigmas(patternSet[:800])
-    centers = centerSigmas['centers']
-    sigmas = centerSigmas['sigmas']
-    
+                
+    #patternSet = parseBlock(lines)
+    #patternSet = parseCar(lines)
+    patternSet = parseFlare(lines)
+       
     print("pats: " + str(len(patternSet)))
     with open(parseSet['outFile'], 'w+') as outfile:
-        data = {'count':len(patternSet[800:]),
+        data = {'count':len(patternSet),
                 'width':parseSet['width'],
                 'height':parseSet['height'],
-                'patterns':patternSet[800:],
-                'centers':centers,
-                'sigmas':sigmas}
+                'patterns':patternSet}
         json.dump(data, outfile)
 
 
