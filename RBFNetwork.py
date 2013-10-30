5294bd2899ffb1f6e0dbc1a37435d415615e32a8
clarkrabel-wf <clark.rabel@Webfilings.com>
2013-10-30 17:02:24 -0600

    small start on training Strategy

--- RBFNetwork.py
+++ RBFNetwork.py
@@ -5,8 +5,8 @@ import sys
 import random
 import math
 import time
-import patternSet
 from patternSet import PatternSet
+import trainingStrategy
 
 eta = 1.00
 
@@ -31,6 +31,16 @@ class NetLayerType:
             self.Input:"I",
             self.Hidden:"H",
             self.Output:"O"}[x]
+
+class LearingMethod:
+    MLP, EvolutionStrategy, GeneticAlgorithm, DifferentialGA = range(4)
+
+    @classmethod
+    def desc(self, x):
+        return {self.MLP: "MLP",
+                self.EvolutionStrategy: "EvolutionStrategy",
+                self.GeneticAlgorithm: "GeneticAlgorithm",
+                self. DifferentialGA: "DifferentialGA"}[x]
 
 # Weights are initialized to a random value between -0.3 and 0.3
 def randomInitialWeight():
@@ -96,80 +106,23 @@ def printPatterns(pattern):
 
 
 class Net:
-    def __init__(self, patternSet, hiddenArch):
+    def __init__(self, patternSet, hiddenArch, learningMethod):
+        self.learningMethod = learningMethod
         self.layers.append(Layer(NetLayerType.Input, None, patternSet.inputMagnitude()))
         for elem in hiddenArch:
             self.layer.append(Layer(NetLayerType.Hidden, self.layers[-1], elem))
         self.layers.append(Layer(NetLayerType.Output, self.layers[-1], patternSet.outputMagnitude()))
         self.patternSet = patternSet
         self.absError = 100
+        self.trainingStrategy = trainingStrategy(learningMethod)
         #self.buildCenters()
 
     # Run is where the magic happens. Training Testing or Validation mode is indicated and
     # the coorisponding pattern set is loaded and ran through the network
     # At the end Error is calculated
     def run(self, mode, startIndex, endIndex):
-        patterns = self.patternSet.patterns
-        eta = 1.0
-        errorSum = 0.0
-        print("Mode[" + PatternType.desc(mode) + ":" + str(endIndex - startIndex) + "]")
-        startTime = time.time()
-        for i in range(startIndex, endIndex):
-            #Initialize the input layer with input values from the pattern
-            # Feed those values forward through the remaining layers, linked list style
-            self.layers[NetLayerType.Input].setInputs(vectorizeMatrix(patterns[i]['p']))
-            self.layers[NetLayerType.Input].feedForward()
-            if mode == PatternType.Train:
-                #For training the final output weights are adjusted to correct for error from target
-                self.layers[NetLayerType.Output].adjustWeights(self.patternSet.targetVector(patterns[i]['t']))
-            else:
-                self.patternSet.updateConfusionMatrix(patterns[i]['t'], self.layers[NetLayerType.Output].getOutputs())
-                # print("Output:")
-                # printPatterns(self.layers[NetLayerType.Output].getOutputs())
-                # print("Target:")
-                # printPatterns(self.patternSet.targetVector(patterns[i]['t']))
-            # Each pattern produces an error which is added to the total error for the set
-            # and used later in the Absolute Error Calculation
-            outError = outputError(self.layers[NetLayerType.Output].getOutputs(), self.patternSet.targetVector(patterns[i]['t']))
-            errorSum = errorSum + outError
-            eta = eta - eta/((endIndex - startIndex)*1.1)
-            # if mode != PatternType.Train and logResults:
-            #     # Logging
-            #     with open('results.txt', 'a') as file:
-            #         out = ""
-            #         for output in self.layers[NetLayerType.Output].getOutputs():
-            #             out = out + str(round(output, 2)) + '\t'
-            #         for target in patterns[i]["outputs"]:
-            #             out = out + str(round(target, 2)) + '\t'
-            #         file.write(out + '\n')
-            # self.recordWeights()
-        endTime = time.time()
-        print("Time [" + str(round(endTime-startTime, 4)) + "sec]")
-        # if mode != PatternType.Train:
-        #     # Calculate Absolute Error pg.398
-        #     self.absError = 1.0/(patCount*len(patterns[0]["outputs"]))*errorSum
-        #     print("Absolute Error: " + str(round(self.absError, 4)) + " [" + str(endTime-startTime) + "]")
-        #     if logError:
-        #         # Logging
-        #         with open('errors.txt', 'a') as file:
-        #             file.write(str(round(self.absError, 4)) + '\t' + str(endTime-startTime) + '\n')
 
-    # During this process we calculate sigma which is used in the Hidden Layers' RBF function
-    def buildCenters(self):
-        centers = self.patternSet.centers
-        neurons = self.layers[NetLayerType.Hidden].neurons
-        n = 0
-        maxEuclidianDistance = 0.0
-        # print("Centers:")
-        keys = list(centers.keys())
-        keys.sort()
-        for key in keys:
-            neurons[n].center = vectorizeMatrix(centers[key])
-            n = n + 1
-        # for n in range(len(centers)):
-        #     printPatterns(neurons[n].center)
 
-    # Logging
     def recordWeights(self):
         self.logWeightIterator = self.logWeightIterator + 1
         if logWeights and self.logWeightIterator%self.logWeightFrequency == 0:
@@ -214,21 +167,6 @@ class Layer:
             out.append(neuron.output)
         return out
 
-    # Adjusting weights is done on the output layer in order to scale the
-    # output of a neuron's RBF function.
-    def adjustWeights(self, targets):
-        if len(targets) != len(self.neurons):
-            raise NameError('Output dimension of network does not match that of target!')
-        # DeltaWkj = (learningRate)sum(TARGETkp - OUTPUTkp)Yjp
-        prevOutputs = self.prev.getOutputs()
-        # print("O:" + str(round(self.neurons[0].output, 2)) + "  T:" + str(round(targets[0], 2)))
-        for k in range(len(self.neurons)):
-            neuron = self.neurons[k]
-            for j in range(len(prevOutputs)):
-                neuron.weightDeltas[j] = eta * (targets[k] - neuron.output) * prevOutputs[j]
-                neuron.weights[j] = neuron.weights[j] + neuron.weightDeltas[j]
-                if neuron.weights[j] > 9999999:
-                    raise NameError('Divergent Weights!')
 
     # Each Layer has a link to the next link in order.  Input values are translated from
     # input to output in keeping with the Layer's function
@@ -267,19 +205,27 @@ class Layer:
 # weights or centers in calculating it's outputs.  Calculations are done
 # in the layer as function of the neuron is tied to the layer it is contained in
 class Neuron:
-    sigma = 0.0
+    idIterator = 0
     
     def __init__(self, layer):
+        self.id = Neuron.idIterator
         self.layer = layer
         self.input = 0.00
         self.output = 0.00
-        self.center = []
         self.weights = []
         self.weightDeltas = []
-        if layer.prev != None:
+        if layer.prev:
             for w in range(len(layer.prev.neurons)):
                 self.weights.append(randomInitialWeight())
                 self.weightDeltas.append(0.0)
+        Neuron.idIterator += 1
+
+    @classmethod
+    def getWeights(n):
+        return Neuron.trainingStrategy.getCurrentMemberWeightForNeuron(n)
+
+    def getWeights(self):
+        return Neuron.getWeights(self)
 
     # Output Format
     def __str__(self):
---
+++ trainingStrategy.py
@@ -0,0 +1,55 @@
+
+
+
+class trainingStrategy(object):
+
+    def __init__(self, strategy):
+        self.strategy = strategy
+        self.genome =
+
+    def nextGeneration(self):
+
+    def train(self):
+
+
+class MLP(trainingStrategy):
+    def __init__(self, strategy):
+        super(self.__class__, self).__init__(strategy)
+
+    def backprop(self):
+
+class EvolutionStrategy(trainingStrategy):
+    def __init__(self, strategy):
+        super(self.__class__, self).__init__(strategy)
+
+    def mutate(self):
+
+    def crossover(self):
+
+    def select(self):
+
+    def repopulate(self):
+
+class GeneticAlgorithm(trainingStrategy):
+    def __init__(self, strategy):
+        super(self.__class__, self).__init__(strategy)
+
+    def mutate(self):
+
+    def crossover(self):
+
+    def select(self):
+
+    def repopulate(self):
+
+class DifferentialGA(trainingStrategy):
+    def __init__(self, strategy):
+        super(self.__class__, self).__init__(strategy)
+
+    def mutate(self):
+
+    def crossover(self):
+
+    def select(self):
+
+    def repopulate(self):
